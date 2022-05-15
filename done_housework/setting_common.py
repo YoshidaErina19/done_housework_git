4d66a5c73950cdacb42d4c5a15a2c35f03f3bdaa
yoshida_erina <yoshida.erina.19@gmail.com>
2022-05-15 16:59:46 +0900

    カスタムユーザーモデルを登録、マイグレーション、認証用ルーティング追加

--- done_housework/setting_common.py
+++ done_housework/setting_common.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
 
     'housework.apps.HouseworkConfig',
     'django_bootstrap5',
+    'accounts.apps.AccountsConfig',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
@@ -137,3 +142,34 @@ MESSAGE_TAGS = {
     messages.SUCCESS: 'alate alate-success',
     messages.INFO: 'alate alale-info',
 }
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# django-allauthで利用するdjango.contrib.sitesを使うためにサイト識別用IDを設定
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    'allauth.account.auth_backends.AuthenticationBackend'  # 一般ユーザー用(メールアドレス認証)
+    'django.contrib.auth.backends.ModelBackend'  # 管理ユーザー用(ユーザー名認証)
+)
+
+# メールアドレス認証に変更する設定
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_USERNAME_REQUIRED = False
+
+# サインアップにメールアドレス確認をはさむよう設定
+ACCOUNT_EMAIL_VARIFICATION = 'mandatory'
+ACCOUNT_EMAIL_REQUIRED = True
+
+# ログイン/ログアウト後の遷移先を設定
+LOGIN_REDIRECT_URL = 'housework:index'
+ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
+
+# ログアウトリンクのクリック一発でログアウトする設定
+ACCOUNT_LOGOUT_ON_GET = True
+
+# django-allauthが送信するメールの件名に自動付与される接頭辞をブランクにする設定
+ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
+
+# デフォルトのメール送信元を設定
+DEFAULT_FROM_EMAIL = 'admin@example.com'
