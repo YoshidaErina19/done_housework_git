3b83a230de66d54555423b6ebe3a5c5d14a05eb8
yoshida_erina <yoshida.erina.19@gmail.com>
2022-05-17 08:06:19 +0900

    url.pyを修正

--- done_housework/urls.py
+++ done_housework/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.staticfiles.utls import static
+from django.contrib.staticfiles.urls import static
 from django.urls import path,include
 
 from . import setting_common,setting_dev
