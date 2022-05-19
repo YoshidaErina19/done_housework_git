c8d81a8b91d0d50ced91148e202fda222ef8f902
yoshida_erina <yoshida.erina.19@gmail.com>
2022-05-19 22:08:09 +0900

    日記一覧表示機能のルーティング・ビューを編集

--- housework/urls.py
+++ housework/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'housework'
 urlpatterns = [
     path('',views.IndexView.as_view(),name='index'),
-    path('inquiry/',views.InquiryView.as_view(),name="inquiry")
+    path('inquiry/',views.InquiryView.as_view(),name="inquiry"),
+    path('housework-list', views.HouseworkListView.as_view(), name="housework-list"),
 ]
--- housework/views.py
+++ housework/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 import logging
 
 from django.urls import reverse_lazy
@@ -11,6 +9,10 @@ from django.views import generic
 from .forms import InquiryForm
 
 from django.contrib import messages
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Housework
 
 logger = logging.getLogger(__name__)
 
@@ -32,3 +34,11 @@ class InquiryView(generic.FormView):
         messages.success(self.request,'メッセージを送信しました。')
         logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
         return super().form_valid(form)
+
+class HouseworkListView(LoginRequiredMixin, generic.ListView):
+    model = Housework
+    template_name = 'housework_list.html'
+
+    def get_queryset(self):
+        houseworks = Housework.objects.filter(user=self.request.user).order_by('-created_at')
+        return houseworks
